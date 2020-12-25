bab766501896927b5bb711edea21be6b779356ca
Matthew Gleich <email@mattglei.ch>
2020-12-25 18:14:40 -0500

    ✨ Finalize script

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/Matt-Gleich/logoru v0.0.0-20201216171923-6f5b594893b5
+	github.com/nathany/bobblehat v0.0.0-20170421151738-14b0d1b4643e
 	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
+	"time"
+
 	"github.com/Matt-Gleich/logoru"
+	"github.com/nathany/bobblehat/sense/screen"
+	"github.com/nathany/bobblehat/sense/screen/color"
 )
 
 func main() {
-	logoru.Success("Program booted up!")
+	for {
+		var (
+			x   int
+			y   int
+			fb  = screen.NewFrameBuffer()
+			now = time.Now()
+		)
+
+		for i := 2; i <= 60; i++ {
+			if x == 8 {
+				y++
+				x = 0
+			}
+			if y == 8 {
+				break
+			}
+
+			if i <= now.Minute() {
+				fb.SetPixel(x, y, color.New(255, 255, 255))
+				continue
+			}
+
+			rgbVal := int(4.25 * now.Second())
+			fb.SetPixel(rgbVal, rgbVal, rgbVal)
+
+			x++
+		}
+
+		err := screen.Draw(fb)
+		if err != nil {
+			logoru.Error("Failed to update lights;", err)
+		}
+
+		time.Sleep(time.Millisecond * 20)
+	}
 }
